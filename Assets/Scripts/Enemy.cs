9eb40acb30ab7254a8c86eef3dec8c7d06afdfad
Hyung Seok Jang <hsjang04@gmail.com>
2024-12-13 18:39:39 +0900

    Final
    add props, start scene update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.Callbacks;
+//using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
--- Assets/Scripts/UI_Controller.cs
+++ Assets/Scripts/UI_Controller.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
